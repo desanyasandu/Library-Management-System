88fa877625b72274baaaaf770477672211abf6dc
desanyasandu <desanyasandu@gmail.com>
2026-02-16 23:15:46 +0530

    Visual Elements

--- libraryManagement/resources/views/welcome.blade.php
+++ libraryManagement/resources/views/welcome.blade.php
@@ -4,10 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Library Management System</title>
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased bg-gray-100 dark:bg-gray-900">
